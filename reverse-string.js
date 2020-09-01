8509d5caf3b3459889da448ac532fd9bfce2b02e
hylobates-lar <alison.v.quaglia@gmail.com>
2020-09-01 19:01:27 -0400

    reverse string in place complete

---
+++ reverse-string.js
@@ -0,0 +1,19 @@
+//Write a function that takes an array of characters and reverses the letters in place.
+
+function reverse(arrayOfChars) {
+
+    let leftIndex = 0;
+    let rightIndex = arrayOfChars.length - 1;
+  
+    while (leftIndex < rightIndex) {
+  
+      // Swap characters
+      const temp = arrayOfChars[leftIndex];
+      arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+      arrayOfChars[rightIndex] = temp;
+  
+      // Move towards middle
+      leftIndex++;
+      rightIndex--;
+    }
+}
