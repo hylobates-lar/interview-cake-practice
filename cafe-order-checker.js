2d3bc3eb921c3cf4badf00946402b08a899ae282
hylobates-lar <alison.v.quaglia@gmail.com>
2020-09-20 17:07:22 -0400

    add their solution

--- cafe-order-checker.js
+++ cafe-order-checker.js
@@ -18,7 +18,7 @@ let served = [17, 8, 12, 19, 24, 2]
 // Note: Order numbers are arbitrary. They do not have to be in increasing order.
 
 function orderChecker(takeOut, dineIn, served) {
-    let takeOutCopy = takeOut.slice(0)
+    let takeOutCopy = takeOut.slice(0) // [...takeOut]
     let dineInCopy = dineIn.slice(0)
 
     for (i = 0; i < served.length; i++) {
@@ -35,4 +35,48 @@ function orderChecker(takeOut, dineIn, served) {
         }
     }
     return true
+}
+
+
+//their solution
+function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
+    var takeOutOrdersIndex = 0;
+    var dineInOrdersIndex = 0;
+    var takeOutOrdersMaxIndex = takeOutOrders.length - 1;
+    var dineInOrdersMaxIndex = dineInOrders.length - 1;
+
+    for (var i = 0; i < servedOrders.length; i++) {
+        var order = servedOrders[i];
+
+        // if we still have orders in takeOutOrders
+        // and the current order in takeOutOrders is the same
+        // as the current order in servedOrders
+        if (takeOutOrdersIndex <= takeOutOrdersMaxIndex &&
+                order === takeOutOrders[takeOutOrdersIndex]) {
+            takeOutOrdersIndex++;
+
+        // if we still have orders in dineInOrders
+        // and the current order in dineInOrders is the same
+        // as the current order in servedOrders
+        } else if (dineInOrdersIndex <= dineInOrdersMaxIndex &&
+                order === dineInOrders[dineInOrdersIndex]) {
+            dineInOrdersIndex++;
+
+        // if the current order in servedOrders doesn't match the current
+        // order in takeOutOrders or dineInOrders, then we're not serving first-come,
+        // first-served
+        } else {
+            return false;
+        }
+    }
+
+    // check for any extra orders at the end of takeOutOrders or dineInOrders
+    if (dineInOrdersIndex != dineInOrders.length ||
+           takeOutOrdersIndex != takeOutOrders.length) {
+        return false;
+    }
+
+    // all orders in servedOrders have been "accounted for"
+    // so we're serving first-come, first-served!
+    return true;
 }
